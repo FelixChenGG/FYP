885811dc7a58e541dad4076e6e990ee976b91554
FelixChenGG <75570618+FelixChenGG@users.noreply.github.com>
2022-04-17 12:41:05 +0800

    update

--- src/component/Record/AccessRecord.js
+++ src/component/Record/AccessRecord.js
@@ -1,27 +1,58 @@
-import React,{Component,useState,useEffect} from 'react'
+import React,{useState} from 'react'
 import { Card, Table,Pagination} from 'react-bootstrap'
 
 
 
-const addItem=(props)=>{
-    for(var i=0 ; i <= props.records; i++){
-        <Pagination.Item>{i}</Pagination.Item>
-    }
+// const addItem=(props)=>{
+//     for(var i=0 ; i <= props.records; i++){
+//         <Pagination.Item>{i}</Pagination.Item>
+//     }
+// }
+
+
+function Paginations({ postsPerPage, totalPosts, paginate }) {
+
+    const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  console.log(postsPerPage, totalPosts, paginate )
+  return (
+    <nav>
+      <Pagination>
+        {pageNumbers.map(number => (
+          <Pagination.Item key={number}>
+            <a onClick={() => paginate(number)} href='!#' className='page-link'>
+              {number}
+            </a>
+            </Pagination.Item>
+        ))}
+      </Pagination>
+    </nav>
+  );
 }
 
+
+
 function AccessRecord() {
-    
-    const [records, setRecords] = useState([ {datatime:"1" },{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" }
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(10);
+    const currentPosts=[ {datatime:"1" },{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" }
                                             ,{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" }
-                                            ,{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" }])
+                                            ,{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" },{datatime:"2" }]
         // useEffect(() => {
         //     fetch('http://127.0.0.1:5001/record/all')
         //     .then(res => res.json())
         //     .then(data => setRecords(data.data))
         // }, [records]);
     
-
-        
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const records = currentPosts.slice(indexOfFirstPost, indexOfLastPost)
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber);
 
    //setRecords(data)
         return (
@@ -48,15 +79,11 @@ function AccessRecord() {
                         </Table>
                     </Card.Body>
                     <Card.Footer className='bg-white' >
-                         <Pagination
-                          dataLimit={10}
-                          size={
-                            Math.ceil(records.length/10)}
-                          >
-                         <Pagination.First />
-                         <addItem records={records.length/10}/>
-                         <Pagination.Last />
-                         </Pagination>
+                    <Paginations
+                        postsPerPage={postsPerPage}
+                        totalPosts={currentPosts.length}
+                        paginate={paginate}
+                    />
                     </Card.Footer>
                     </Card></>
     )
